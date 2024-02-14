e5b8b2651cccf2f15877786bcac61ac39088bf6a
micalh90 <micalh90@gmail.com>
2024-02-13 23:22:36 -0500

    icon

--- a_promo_viewer/urls.py
+++ a_promo_viewer/urls.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 from django.urls import path, include
 
+
 urlpatterns = [
     path('', include('b_landing_page.urls')),
     path('admin/', admin.site.urls, name='admin_page'),
+
 ]
